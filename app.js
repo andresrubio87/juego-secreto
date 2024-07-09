370b7ef01fb80bef040ad3f92366538c9ec1ff85
andresrubio87 <andresrubio87@gmail.com>
2024-07-09 16:10:41 -0500

    Add files via upload
    Ultima versión Juego número secreto

---
+++ app.js
@@ -0,0 +1,110 @@
+
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+let listaNumeroDeUSuario = [];
+
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        listaNumeroDeUSuario.push(numeroDeUsuario);
+        console.log(`Los valores probados han sido: ${listaNumeroDeUSuario}`);
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(`el número secreto es: ${numeroGenerado}`);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales();
+
+
+/*
+Crea una lista vacía llamada "listaGenerica".
+Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los siguientes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
+Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 'Java', 'Ruby' y 'GoLang'.
+Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion.
+Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion en orden inverso.
+Crea una función que calcule el promedio de los elementos en una lista de números.
+Crea una función que muestre en la consola el número más grande y el número más pequeño en una lista.
+Crea una función que devuelva la suma de todos los elementos en una lista.
+Crea una función que devuelva la posición en la lista donde se encuentra un elemento pasado como parámetro, o -1 si no existe en la lista.
+Crea una función que reciba dos listas de números del mismo tamaño y devuelva una nueva lista con la suma de los elementos uno a uno.
+Crea una función que reciba una lista de números y devuelva una nueva lista con el cuadrado de cada número.
+let listaGenerica = [];
+let listaLenguajesDeProgramacion = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+listaLenguajesDeProgramacion.push("Java", "Ruby", "GoLang")
+
+function mostrarElementos(lista) {
+    console.log (lista)
+};
+
+mostrarElementos(listaLenguajesDeProgramacion);
+
+// Mostrar la lista en orden inverso
+for (let i = listaLenguajesDeProgramacion.length - 1; i >= 0; i--) {
+  console.log(listaLenguajesDeProgramacion[i]);
+}
+*/
